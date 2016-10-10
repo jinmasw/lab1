17d40c143aa14321de708d8e4b7764718e19d50b
jinmasw <jinmasw@gmail.com>
2016-10-10 13:18:44 +0200

    Add files via upload

--- RegistrarConFoto.php
+++ RegistrarConFoto.php
@@ -1,30 +1,40 @@
 <?php
 
-//$serv = "mysql.hostinger.es";
-//$usuario = "u311047301_admin";
-//$password = "admin123";
-//$bd = "u311047301_quiz";
+$serv = "mysql.hostinger.es";
+$usuario = "u311047301_admin";
+$password = "admin123";
+$bd = "u311047301_quiz";
 
-$serv = "127.0.0.1";
-$usuario = "root";
-$password = "";
-$bd = "quiz";
+//$serv = "127.0.0.1";
+//$usuario = "root";
+//$password = "";
+//$bd = "quiz";
 
-$esp = $_POST['especialidad'];
-if ($esp == 'Otro'){
-	$esp = $_POST['otra'];
-}
-
-$foto = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
-
-$link = mysqli_connect($serv,$usuario,$password,$bd);
-$sql = "INSERT INTO usuario VALUES ('$_POST[nombre]','$_POST[apellido_1]','$_POST[apellido_2]','$_POST[correo]','$_POST[telefono]','$_POST[contraseña]','$esp','$_POST[area_de_texto]','$foto')";
-if (!mysqli_query($link ,$sql))
+$val="$_POST[correo]";
+$opt=array("options"=>array("regexp"=>"/[a-zA-z]+[0-9]{3}(@ikasle.ehu.e)u?(s)/"));
+if(filter_var($val, FILTER_VALIDATE_REGEXP,$opt))
 {
-	die('Error:' . mysqli_error($link));
-}
+	$esp = $_POST['especialidad'];
+	if ($esp == 'Otro'){
+	$esp = $_POST['otra'];
+	}
 
-echo "1 record added";
-echo "<p> <a href='VerUsuariosConFoto.php'> Ver usuarios </a>";
-mysqli_close($link);
+	$foto = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+
+	$link = mysqli_connect($serv,$usuario,$password,$bd);
+	$sql = "INSERT INTO usuario VALUES ('$_POST[nombre]','$_POST[apellido_1]','$_POST[apellido_2]','$_POST[correo]','$_POST[telefono]','$_POST[contraseña]','$esp','$_POST[area_de_texto]','$foto')";
+	if (!mysqli_query($link ,$sql))
+	{
+		die('Error:' . mysqli_error($link));
+	}
+
+	echo "1 record added";
+	echo "<p> <a href='VerUsuariosConFoto.php'> Ver usuarios </a>";
+	mysqli_close($link);
+}
+else
+{
+	echo "Introduce un email valido";
+	echo "<p> <a href='registro.html'> Volver a la pagina de registro </a>";
+}
 ?>
--- VerUsuariosConFoto.php
+++ VerUsuariosConFoto.php
@@ -1,14 +1,14 @@
 <?php
 
-//$serv = "mysql.hostinger.es";
-//$usuario = "u311047301_admin";
-//$password = "admin123";
-//$bd = "u311047301_quiz";
+$serv = "mysql.hostinger.es";
+$usuario = "u311047301_admin";
+$password = "admin123";
+$bd = "u311047301_quiz";
 
-$serv = "127.0.0.1";
-$usuario = "root";
-$password = "";
-$bd = "quiz";
+//$serv = "127.0.0.1";
+//$usuario = "root";
+//$password = "";
+//$bd = "quiz";
 
 $link = mysqli_connect($serv, $usuario, $password, $bd);
 $usuarios = mysqli_query($link, "select * from usuario" );
